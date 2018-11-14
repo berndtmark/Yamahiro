cec155fc54cf2859dd899a65d65b54c49f9a1c8c
Mark Berndt <berndtmark@gmail.com>
2018-11-14 22:00:28 +0000

    improvments

--- Yamahiro.js
+++ Yamahiro.js
@@ -1,32 +1,27 @@
-const Slimbot = require('slimbot');
+// removes irrelevant warning
+process.env.NTBA_FIX_319 = 1;
+
+const telegramBot = require('node-telegram-bot-api');
 const chokidar = require('chokidar');
 const path = require("path");
 const config = require("./config.json");
 
-const slimbot = new Slimbot(config.token);
+const bot = new telegramBot(config.token, {polling: true});
 
-let initFileWatcher = () => {
-    const watcher = chokidar.watch(config.dir, {
-        ignored: /[\/\\]\./, persistent: true, ignoreInitial: true
+const watcher = chokidar.watch(config.dir, {
+    ignored: /[\/\\]\./, persistent: true, ignoreInitial: true
+});
+
+watcher
+    .on('ready', () => { 
+        console.log('Initial scan complete.');
+    })
+    .on('error', error => { 
+        console.warn(`File watch error: ', ${error}`);
+    })
+    .on('add', file => { 
+        let message = `${path.basename(file)}`;
+        bot.sendMessage(config.chatId, message);
     });
 
-    watcher
-        .on('ready', () => { 
-            console.log('Initial scan complete.');
-        })
-        .on('error', error => { 
-            console.warn(`File watch error: ', ${error}`);
-        })
-        .on('add', file => { 
-            let message = `${path.basename(file)}`;
-            slimbot.sendMessage(config.chatId, message);
-        });
-}
-
-let startUp = () => {
-    console.log(`Connected!`);
-    initFileWatcher();
-}
-
-startUp();
-slimbot.startPolling();
+console.log(`Connected!`);
